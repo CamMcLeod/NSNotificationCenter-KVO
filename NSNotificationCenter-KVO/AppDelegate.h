b4f253eea10e762a013aa7c762731699119bae80
Cam McLeod <cammcleod3@gmail.com>
2019-06-09 12:06:19 -0700

    Initial Commit

---
+++ NSNotificationCenter-KVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSNotificationCenter-KVO
+//
+//  Created by Cameron Mcleod on 2019-06-09.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
